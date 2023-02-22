3083e95e39c8f7d327382af70b618e0da3f28f79
ErastusMunala <munalaerastus@gmail.com>
2023-02-22 08:51:59 -0800

    another function fix

--- shell_loop.c
+++ shell_loop.c
@@ -60,11 +60,9 @@ int find_builtin(info_t *info)
 		{"exit", _myexit},
 		{"env", _myenv},
 		{"help", _myhelp},
-		{"history", _myhistory},
 		{"setenv", _mysetenv},
 		{"unsetenv", _myunsetenv},
 		{"cd", _mycd},
-		{"alias", _myalias},
 		{NULL, NULL}
 	};
 
